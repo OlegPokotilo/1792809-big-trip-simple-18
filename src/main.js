b84c1fe8796e87623460c3e4b7fc0f38d2b333c3
Олег <exdns@mail.ru>
2022-08-14 00:48:51 +0300

    Создаю модули модули в рамках MVP

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
-/* import NewTaskButtonView from './view/new-task-button-view.js';
+import FiltersView from './view/filters-view.js';
+import SortingView from './view/sorting-view.js';
+import CreationFormView from './view/creationForm-view.js';
+import FormEditorView from './view/editForm-view.js';
 import {render} from './render.js';
 
-const siteMainElement = document.querySelector('.main');
-const siteHeaderElement = siteMainElement.querySelector('.main__control');
+const siteMainElement = document.querySelector('.page-body');
+const siteFiltersElement = siteMainElement.querySelector('.trip-controls__filters');
+const siteTripEventsElement = siteMainElement.querySelector('.trip-events');
 
-render(new NewTaskButtonView(), siteHeaderElement); */
-
-console.log('Hello');
-
+render(new FiltersView(), siteFiltersElement);
+render(new SortingView(), siteTripEventsElement);
+render(new CreationFormView(), siteTripEventsElement);
+render(new FormEditorView(), siteTripEventsElement);
