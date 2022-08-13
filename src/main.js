cd87e387e715bc3faf6f808cbdff0607ba4545be
Олег <exdns@mail.ru>
2022-08-13 10:30:25 +0300

    Устанавливаю webpack

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
-import NewTaskButtonView from './view/new-task-button-view.js';
+/* import NewTaskButtonView from './view/new-task-button-view.js';
 import {render} from './render.js';
 
 const siteMainElement = document.querySelector('.main');
 const siteHeaderElement = siteMainElement.querySelector('.main__control');
 
-render(new NewTaskButtonView(), siteHeaderElement);
+render(new NewTaskButtonView(), siteHeaderElement); */
+
+console.log('Hello');
+
